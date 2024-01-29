e3375163f7b8616316d93971132823c4679875a0
m091u <mirabelatiugan@gmail.com>
2024-01-29 17:42:30 +0100

    'session2 practice'

---
+++ pythonProject1/my_turtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle1 = turtle.Turtle()
+turtle2 = turtle.Turtle()
+
+turtle1.color('red')
+turtle1.pensize(20)
+turtle1.shape('turtle')
+turtle1.penup()
+turtle1.goto(0, 50)
+
+turtle2.color('pink')
+turtle2.pensize(15)
+turtle2.shape('turtle')
+turtle2.penup()
+turtle2.goto(0, -50)
+
+turtle.done()
+
---
+++ pythonProject1/session2_extra.py
@@ -0,0 +1,67 @@
+print("foo", 42, "bar")
+
+print("foo", 42, "bar", sep="/")  #adding a separator
+
+d = {"foo": 1, "bar": 2, "baz": 3}
+for k, v in d.items():
+    print(k, v, sep=" -> ")
+
+for number in range(10):
+    print(number, end=(" " if number < 9 else "\n"))
+
+
+# name = input("What is your name? ")
+# age = input('what is your age')
+# print(f"Hello, {name}. You are {age}.")
+
+# for loops
+a = ['foo', 'bar', 'baz']
+for i in a:
+    print(i+'and')
+
+print(iter('foobar'))
+
+itr = iter(a)
+print(set(itr))
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+
+# iterating through a dictionary
+d = {'foo': 1, 'bar': 2, 'baz': 3}
+for k in d:
+    print(f'printing {k} and {d[k]}')
+
+# iterate through a dictionary accessing values
+for v in d.values():
+    print(f'printing values {v} only')
+
+# iterate through a dictionary accessing both the keys and values
+for k, v in d.items():
+    print(f'printing both key {k} and value {v}')
+
+# iterating through a range
+x = range(5)
+print(tuple(x))
+
+for i in x:
+    print(i)
+
+# range(<begin>, <end>, <stride>) returns an iterable that yields integers starting with <begin>, up to but not including <end>.
+# If specified, <stride> indicates an amount to skip between values
+f = list(range(5, 20, 3))
+
+print(f)
+
+for i in ['foo', 'bar', 'baz', 'qux']:
+    if 'x' in i:
+        break
+    print(i)
+
+
+for i in ['foo', 'bar', 'baz', 'qux']:
+    if i == 'bal':
+       break
+    print(f'iteration with break {i}')
+else:
+    print('Done.')
