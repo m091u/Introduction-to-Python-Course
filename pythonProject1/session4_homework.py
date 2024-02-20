d39bedb008b91db8f92bfa71677b905ad1d0791a
m091u <mirabelatiugan@gmail.com>
2024-02-20 21:56:55 +0100

    'session4 homework'

--- pythonProject1/session4_homework.py
+++ pythonProject1/session4_homework.py
@@ -1,4 +1,4 @@
-# Question 1: index starts at 0
+# Question 1: mistake-> wrong index, for the first item index is 0
 import random
 
 shopping_list = [
@@ -8,7 +8,6 @@ shopping_list = [
     "long-grain rice",
     "cheese board",
 ]
-
 print(shopping_list[0])
 
 # Question 2
@@ -26,5 +25,28 @@ else:
     print('Sorry, we do not have that chocolate.')
 
 # Question 3 lottery
-lottery_ticket = [8, 15, 25, 39, 46, 28, 19]
-lottery_numbers = random.choice()
+your_lottery_ticket = [8, 15, 25, 39, 46, 28, 19]
+lottery_numbers = [random.choice(range(1, 50)) for _ in range(7)]
+
+matches = 0
+
+for number in your_lottery_ticket:
+    if number in lottery_numbers:
+        matches += 1
+
+if matches == 3:
+    prize = 20
+elif matches == 4:
+    prize = 40
+elif matches == 5:
+    prize = 100
+elif matches == 6:
+    prize = 10000
+elif matches == 7:
+    prize = 1000000
+else:
+    prize = 0
+
+print(f"Your lottery numbers: {your_lottery_ticket}")
+print(f"Draw numbers: {lottery_numbers}")
+print(f"Matching numbers: {matches} and prize: £{prize}")
